afc5ae05182a2748e89d651c84a681e51a9c24c8
tpham13 <tpham0123@gmail.com>
2021-05-14 00:46:27 -0700

    Complete Computer class

--- lib/game.rb
+++ lib/game.rb
@@ -26,12 +26,13 @@ class Game
 
     def won? 
         #want to check if in each cell(i.e: [0, 1, 2]) has the same character
-        WIN_COMBINATIONS.find do |winner|
+        WIN_COMBINATIONS.find do |combo|
             #check for the character in the 1st position/index of the cell is = to the 2nd position
-            @board.cells[winner[0]] == @board.cells[winner[1]] && 
+            @board.cells[combo[0]] == @board.cells[combo[1]] && 
             #check for the character in the 2nd position/index of the cell is = to the 3rd position
-            @board.cells[winner[1]] == @board.cells[winner[2]] && 
-            (@board.cells[winner[0]] == "X" || @board.cells[winner[0]] == "O") 
+            @board.cells[combo[1]] == @board.cells[combo[2]] && 
+            #check for which token("X" or "O") won 
+            (@board.cells[combo[0]] == "X" || @board.cells[combo[0]] == "O") 
         end 
     end  
 
@@ -53,5 +54,32 @@ class Game
             #then the winner will be whatever token("X" or "O") in that first position/index
             @winner = @board.cells[winning_combo.first]
         end 
+    end 
+
+    def turn
+        player = current_player
+        current_move = player.move(@board)
+        #valid_move? from class Board
+        if !@board.valid_move?(current_move)
+            turn 
+        else 
+            puts "Turn: #{@board.turn_count+1}\n"
+            @board.display 
+            @board.update(current_move, player)
+            puts "#{player.token} move #{current_move}"
+            @board.display
+            puts "\n\n"
+        end 
+    end 
+
+    def play 
+        while !over?
+            turn
+        end
+        if won?
+            puts "Congratulations #{winner}!"
+        elsif draw? 
+            puts "Cat's Game!"
+        end
     end 
 end 

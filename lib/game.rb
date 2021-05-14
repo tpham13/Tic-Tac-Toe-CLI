e101aac4a6004927b9c5fbd79d8185ddd5f6aa8e
tpham13 <tpham0123@gmail.com>
2021-05-14 00:28:03 -0700

    Updated won? method

--- lib/game.rb
+++ lib/game.rb
@@ -24,22 +24,34 @@ class Game
         @board.turn_count % 2 == 0 ? player_1 : player_2
     end 
 
+    def won? 
+        #want to check if in each cell(i.e: [0, 1, 2]) has the same character
+        WIN_COMBINATIONS.find do |winner|
+            #check for the character in the 1st position/index of the cell is = to the 2nd position
+            @board.cells[winner[0]] == @board.cells[winner[1]] && 
+            #check for the character in the 2nd position/index of the cell is = to the 3rd position
+            @board.cells[winner[1]] == @board.cells[winner[2]] && 
+            (@board.cells[winner[0]] == "X" || @board.cells[winner[0]] == "O") 
+        end 
+    end  
+
+    def draw? 
+        @board.full? && !won? 
+    end 
+
     def over?
         #has someone won? or draw?
         #if won is true, it evaluate to true, if draw is true, it will evaluate to true
         won? || draw? 
     end 
 
-    def won? 
-        #want to check if in each cell has the same character
-        WIN_COMBINATIONS.detect do |winner|
-            @board.cells[winner[0]] == @board.cells[winner[1]] && 
-            @board.cells[winner[1]] == @board.cells[winner[2]]
-            #we want to see if the 2nd cell is equal to the 3 cell 
+    def winner 
+        #if a win combination = won? then we want to return 
+        #the piece("X" or "O") that won 
+        #if winning combo return true
+        if winning_combo = won? 
+            #then the winner will be whatever token("X" or "O") in that first position/index
+            @winner = @board.cells[winning_combo.first]
         end 
-    end  
-
-    def draw? 
-        @board.full? && !won? 
     end 
 end 
